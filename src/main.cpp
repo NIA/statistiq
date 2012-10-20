392547f7009a631f7b2e35008774920e30fa89f5
Ivan Novikov <nia.afti@gmail.com>
2012-10-20 20:00:58 +0400

    Fixed icons for distributed version

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include <QtGui/QApplication>
 #include <QtCore/QTranslator>
+#include <QFileInfo>
 #include "mainwindow.h"
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
+    a.addLibraryPath( a.applicationDirPath() + "/plugins" );
 
     QTranslator translator;
     translator.load("statistiq_" + QLocale::system().name());
