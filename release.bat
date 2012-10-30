1144915c5a599ed451434ce0124783dbb3dc3ed5
Ivan Novikov <nia.afti@gmail.com>
2012-10-30 13:46:04 +0400

    Added "release" bat-script
    It copies translation and exe file into appropriate folders

---
+++ release.bat
@@ -0,0 +1,9 @@
+@echo Copying translations...
+copy statistiq_ru.qm ..\statistiq-build-debug\
+copy statistiq_ru.qm ..\statistiq-build-release\
+copy statistiq_ru.qm ..\statistiq-redist\
+
+@echo Copying executable...
+copy ..\statistiq-build-release\release\statistiq.exe ..\statistiq-redist\
+
+@pause
